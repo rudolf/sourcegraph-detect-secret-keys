26f5a342810acc6a591260d1878b6219e6c19363
Rudolf Meijering <skaapgif@gmail.com>
2019-01-26 13:18:54 +0100

    Improve matching and adds hover tooltip  - Match secrets in backtick quoted strings  - Display hover tooltip rather than appending message to end of line

--- src/detect-secret-keys.ts
+++ src/detect-secret-keys.ts
@@ -1,5 +1,5 @@
 import * as sourcegraph from 'sourcegraph';
-import { app, CodeEditor, Range, Window } from 'sourcegraph'
+import { app, CodeEditor, Range, Window } from 'sourcegraph';
 
 async function sleep(milliseconds: number): Promise<void> {
     return new Promise<void>(resolve => setTimeout(resolve, milliseconds))
@@ -44,34 +44,37 @@ function entropy(str: string): number {
   return entropy;
 }
 
-const QUOTED_STRING_REGEXP = /(["'])(?:\\?.)*?\1/ // From http://blog.stevenlevithan.com/archives/match-quoted-string
+const QUOTED_STRING_REGEXP = /(["'`])(?:\\?.)*?\1/g // From http://blog.stevenlevithan.com/archives/match-quoted-string
+
+function detectSecrets(line: string):RegExpExecArray[] {
+    const secrets: RegExpExecArray[] = [];
+    let quoteMatch = QUOTED_STRING_REGEXP.exec(line);
+
+    while (quoteMatch !== null) {
+        if (entropy(quoteMatch[0]) > 3) {
+            secrets.push(quoteMatch);
+        }
+        quoteMatch = QUOTED_STRING_REGEXP.exec(line);
+    }
+    return secrets;
+}
 
 async function decorate(editor: CodeEditor): Promise<any> {
     const decorations = editor.document.text
         .split('\n')
-        .map((line, lineNumber) => {
-            const secrets = [];
-            let quoteMatch = QUOTED_STRING_REGEXP.exec(line);
-            while (quoteMatch !== null) {
-                if (entropy(quoteMatch[0]) > 3) {
-                    secrets.push(quoteMatch);
-                }
-                quoteMatch = QUOTED_STRING_REGEXP.exec(line);
-            }
-
-            return {content: line, lineNumber, secrets};
-        })
+        .map((line, lineNumber) => (
+            {content: line, lineNumber, secrets: detectSecrets(line)}
+        ))
         .filter(line => line.secrets.length > 0)
         .map(({content, lineNumber, secrets}) => ({
-            // TODO: Highlights the whole line even when the start and end character doesn't contain the whole line
-            // TODO: Only creates range for first secret in a line
-            range: new Range(lineNumber, secrets[0].index!, lineNumber, secrets[0][0].length), // -1 because lines are 0 indexed
+            // TODO: Only creates a range for the first secret in a line. Once Sourcegraph supports intra-line ranges this should be updated.
+            range: new Range(lineNumber, secrets[0].index!, lineNumber, secrets[0][0].length),
             border: 'solid',
             borderWidth: '0 0 0 10px',
             borderColor: 'red',
             backgroundColor: 'hsla(0,100%,50%, 0.2)',
             after: {
-                contentText: '🔐 High entropy string, might contain secrets.'
+                contentText: '🔐'
             }
         })
     )
@@ -82,25 +85,40 @@ export function activate(ctx: sourcegraph.ExtensionContext): void {
 
     async function initialize(): Promise<any> {
         const editor = await activeEditor()
-
-        //await initializeSettings()
-
-        //if(configuration.get<Settings>().get('dockerfilelint.enabled')) {
-        //   debugger
         return decorate(editor);
-        //}
     }
 
-//    ctx.subscriptions.add(
-//        sourcegraph.languages.registerHoverProvider(['*'], {
-//            provideHover: (doc, pos) => ({
-//                contents: {
-//                    value: 'Hello world from detect-secret-keys! 🎉🎉🎉',
-//                    kind: sourcegraph.MarkupKind.Markdown
-//                }
-//            }),
-//         })
-//     )
+   ctx.subscriptions.add(
+       sourcegraph.languages.registerHoverProvider(['*'], {
+           provideHover: (doc, pos) => {
+               /*
+                * If Sourcegraph extensions could highlight sub-ranges it would be nice to only provide a hover tooltip
+                * if you're hovering over a secret string.
+                */
+
+            //    const isHoveringOverSecret: boolean = detectSecrets(doc.text.split('\n')[pos.line]).length > 0;
+            //     .map(secret => {
+            //         const secretStartPos = new sourcegraph.Position(pos.line, secret.index);
+            //         const secretEndPos = new sourcegraph.Position(pos.line, secret.index + secret[0].length);
+            //         return pos.isAfterOrEqual(secretStartPos) && pos.isBeforeOrEqual(secretEndPos);
+            //     })
+            //     .some(posInSecret => posInSecret);
+
+                // Sourcegraph highlights the entire line that contains a secret, so hovering anywhere on a line that contains 1 or
+                // more secrets should explain to a user what the highlight is about.
+                const isHoveringOverSecret: boolean = detectSecrets(doc.text.split('\n')[pos.line]).length > 0;
+
+                if (isHoveringOverSecret) {
+                    return {contents: {
+                        value: '⚠️ 🔐 Line contains high entropy string. Check for sensitive information such as secrets or API keys.',
+                        kind: sourcegraph.MarkupKind.Markdown
+                    }}
+                } else {
+                    return null;
+                }
+           },
+        })
+    )
 
     initialize();
 }
